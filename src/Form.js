4dce05f36164ce2b689c9cb1cb1051cf3ffa18ad
blameleo <leonardgre@gmail.com>
2023-02-10 17:57:27 +0000

    done with app

--- src/Form.js
+++ src/Form.js
@@ -1,51 +1,85 @@
 import React from "react";
-import meme from "./memeData.js"
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 
 export default function Form() {
+  const [memeImg, setMemeImg] = useState();
 
-  const [memeImg, setMemeImg] = useState(' ')
+  // setMemeImg("https://i.imgflip.com/3i7p.jpg")
 
-let imgEl
+  // for (let i = 0; i <meme.data.memes.length; i++ ){
+  // }
 
-   let clicked = () => {
-        const rndInt = Math.floor(Math.random() * meme.data.memes.length) 
+  const [memesArray, setMemesArray] = useState();
 
- 
-            let newMeme = meme.data.memes[rndInt].url;
+  function handleClick() {
+    let ranNum = Math.floor(Math.random() * memesArray.length);
 
-            setMemeImg(newMeme)
-    
-            imgEl.innerHtml = `<img className=" mt-5 px-20 pb-20  w-full " src=${memeImg} alt='af'/>`
-        }
+    let randomImageUrl = memesArray[ranNum].url;
+    setMemeImg(randomImageUrl);
+  }
 
-        console.log(imgEl);
+  const [form, setFormData] = useState({
+    topMeme: "",
+    bottomMeme: "",
+  });
 
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => {
+        setMemesArray(data.data.memes);
+      });
+  }, []);
+
+  function handleChange(e) {
+    let { name, value } = e.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
   return (
-  <div>
-      <div className="grid grid-cols-2 gap-4     mt-5">
+    <div>
+      <form
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+        className="grid grid-cols-2 gap-4     mt-5"
+      >
         <input
           className="border p-4 rounded-lg "
           type="text"
           placeholder="top meme"
+          name="topMeme"
+          value={form.topMeme}
+          onChange={handleChange}
         />
         <input
           className="border p-4 rounded-lg "
           type="text"
           placeholder="bottom meme"
+          name="bottomMeme"
+          value={form.bottomMeme}
+          onChange={handleChange}
         />
 
-        <button onClick={clicked} className="col-span-2 bg-purple-500 text-white  mt-5 py-2 hover:bg-purple-700">
+        <button
+          onClick={handleClick}
+          className="col-span-2 bg-purple-500 text-white  mt-5 py-2 hover:bg-purple-700"
+        >
           Get new meme image
         </button>
-          {imgEl}
-  
+      </form>
+
+      <div className="meme mt-10">
+        <h1 className="meme--text top">{form.topMeme}</h1>
+        <img className="meme--image" src={memeImg} alt="" />
+        <h1 className="meme--text bottom">{form.bottomMeme}</h1>
       </div>
-      
-
-  </div>
-
-
+    </div>
   );
 }
